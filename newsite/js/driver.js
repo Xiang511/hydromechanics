aa8777f84414f542bf59be97725a7171f01504f5
LiXiang <toby91511@gmail.com>
2024-02-25 02:46:18 +0800

    Fix : 其他分頁無法開啟側選單

--- newsite/js/driver.js
+++ newsite/js/driver.js
@@ -1,3 +1,16 @@
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
+	})
+});
+
 const driver = window.driver.js.driver;
 
 // const driverObj = driver();
