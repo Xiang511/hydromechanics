44abc172f639dcc5d7d6b935b2c5baad8b3c0e7d
LiXiang <toby91511@gmail.com>
2024-02-25 03:04:27 +0800

    Fix : text error

--- newsite/js/driver.js
+++ newsite/js/driver.js
@@ -29,7 +29,7 @@ const driverObj = driver({
   doneBtnText: "完成",  //最後一步的完成按鈕
   closeBtnText: "關閉",  //關閉按鈕
   steps: [
-    { element: '#sys', popover: { title: 'Hi 我是Xiang', description: '歡迎您的造訪!</br>點選下一步來開始導覽<br>(若不需要此導覽可以點選開白處關閉)', side: "center", align: 'start' } },
+    { element: '#sys', popover: { title: 'Hi 我是Xiang', description: '歡迎您的造訪!</br>點選下一步來開始導覽<br>(若不需要此導覽可以點選關閉)', side: "center", align: 'start' } },
     { element: '.breadcrumb', popover: { title: '先簡單介紹一下網站功能', description: '請點擊下一步', side: "center", align: 'start' } },
     { element: '#AboutUs', popover: { title: '關於我們', description: '介紹有關我們的詳細資訊</br>例如:網站創立原因、與RoyaleApi有何不同', side: "left", align: 'start' } },
     { element: '#LadderRanking', popover: { title: '天梯紀錄', description: '紀錄"傳奇之路最高排名"以及"上一季排名"', side: "left", align: 'start' } },
