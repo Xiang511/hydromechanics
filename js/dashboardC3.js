630df11309012438c2852f5f832e3af83f5abb2f
Xiang511 <toby91511@gmail.com>
2024-05-31 03:00:10 +0800

    feat: 新增 網站資訊版面

--- js/dashboardC3.js
+++ js/dashboardC3.js
@@ -10,20 +10,34 @@ const result = text.substring(text.length - 4);
 const spans = document.querySelectorAll(".sitestatesJs");
 const secondSpan = spans[1];
 const secondSpan_txt = secondSpan.textContent;
-const secondSpan_result = secondSpan_txt.substring(text.length - 4);
+// const secondSpan_result = secondSpan_txt.substring(text.length - 4);
+
+//線上人數
+const online = document.querySelectorAll(".sitestatesJs");
+const onlineSpan = online[2];
+const onlineSpan_txt = onlineSpan.textContent;
+// const onlineSpan_result = onlineSpan_txt.substring(text.length - 4);
+
+//即時計算當月人數
+const arr = [188, 571, 213, 132,518,270,255];
+const Arrresult = arr.reduce((a, b) => a + b);
+month = result - Arrresult
+
+document.getElementById("Total").innerHTML = "累計訪客:"+ result;
+document.getElementById("month").innerHTML = "本月訪客:"+ month;
+document.getElementById("Today").innerHTML = secondSpan_txt;
+document.getElementById("online").innerHTML = onlineSpan_txt;
 
 
 // console顯示
 console.log(text);
 console.log(secondSpan_txt);
+console.log(onlineSpan_txt);
 
 
 
 
-//即時計算當月人數
-const arr = [188, 571, 213, 132,518,270,255];
-const Arrresult = arr.reduce((a, b) => a + b);
-// console.log(Arrresult)
+
 
 
 //首先把數字PUSH到Arr裡面
@@ -31,7 +45,7 @@ const Arrresult = arr.reduce((a, b) => a + b);
 
 var chart = c3.generate({
     size: {
-        height: 430,
+        height: 450,
     },
     padding: {
         top: 30,
